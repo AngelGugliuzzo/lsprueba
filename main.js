b89700d813c4cf5d501baef65f6820adbfc0609b
angelgit <angelgugliuzzo@gmail.com>
2023-04-26 20:33:31 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+//import javascriptLogo from './javascript.svg'
+//import viteLogo from '/vite.svg'
+//import { setupCounter } from './counter.js'
+
+//document.querySelector('#app').innerHTML = `
+//  <div>
+ //   <a href="https://vitejs.dev" target="_blank">
+ //     <img src="${viteLogo}" class="logo" alt="Vite logo" />
+ //   </a>
+ //   <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+ //     <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+ //   </a>
+ //   <h1>Hello Vite!</h1>
+ //   <div class="card">
+ //     <button id="counter" type="button"></button>
+ //   </div>
+ //   <p class="read-the-docs">
+ //     Click on the Vite logo to learn more
+  //  </p>
+ // </div>
+
+
+//setupCounter(document.querySelector('#counter'))
+
+const formulario = document.querySelector('form');
+
+const nombreInput = document.querySelector('#nombre');
+
+const apellidoInput=document.querySelector('#apellido');
+
+const edadInput = document.querySelector('#edad');
+
+const eventoInput = document.querySelector('#nombreEvento');
+
+const paisInput=document.querySelector('#pais');
+
+const cantidadInput=document.querySelector('#cantDePersonas');
+
+const guardarBtn = document.querySelector('#guardar');
+
+//const borrarBtn = document.querySelector('#borrar');
+const listaDePaises = document.querySelector('#listaDePaises');
+
+
+
